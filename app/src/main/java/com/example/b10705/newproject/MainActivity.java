8e82b651b9f2f38a9139d393d0d07146fc653c3e
1494037 <chaesu98@naver.com>
2017-12-19 14:01:50 +0900

    test2
    123123

--- app/src/main/java/com/example/b10705/newproject/MainActivity.java
+++ app/src/main/java/com/example/b10705/newproject/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.b10705.newproject;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     public DBHelper mDbHelper1;
@@ -12,5 +17,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDbHelper1 = new DBHelper(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //item선택됬을때 --> 상단 메뉴
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_store:
+                Intent intent = new Intent(getApplicationContext(), addMenu.class);
+                startActivity(intent);
+                return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onTitleSelected(int i) {
+        if (getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE) {
+            //DetailsFragment detailsFragment = new DetailsFragment();
+            detailFragment detailfragment = new detailFragment();
+            detailfragment.setSelection(i);
+            getSupportFragmentManager().beginTransaction().replace(R.id.details, detailfragment).commit();
+        } else {
+            Intent intent = new Intent(this, detailActivity.class);
+            intent.putExtra("index", i);
+            startActivity(intent);
+
+        }
     }
 }
